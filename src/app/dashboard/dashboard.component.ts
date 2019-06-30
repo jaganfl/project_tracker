d3f8df5e5d5e3ad6a3a9d381fc0af84f7ba50132
jaganfl <jaganfreelancing@gmail.com>
2019-06-30 23:28:38 +0530

    no project message

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProjectListService } from '../services/project-list.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
   projectList = []
+  constructor(
+    
+    private projectListService: ProjectListService
+  ) {}
+  ngOnInit() {
+    this.projectList = this.projectListService.getProjects();
+  }
 }
